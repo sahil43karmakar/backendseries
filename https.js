46f35c25a0b22862151e702ff3c665adfe9cd409
Sahil karmakar <sahil43karmakar@gmail.com>
2025-12-09 21:15:58 +0530

    Added url module

---
+++ https.js
@@ -0,0 +1,46 @@
+const http=require('http');
+const fs=require('fs');
+
+//using the hhtp we can create a server which listens to requests and sends responses
+//below we create a web server using createServer method of http module
+//now we need a handler function which will handle requests and responses and process the request 
+//createServer method takes a callback function as parameter which has two parameters request  and respond 
+//  fs.appendFile("requests.txt", log, (err, data) => { ... })
+// - You’re using Node.js’s File System (fs) module.
+// - appendFile means: take the string in log and add it to the file requests.txt.
+// - If the file doesn’t exist, Node will create it.
+// - If it exists, Node will add the new log entry at the end.
+// - This is asynchronous (non-blocking). The callback (err, data) => { ... } runs only after the file operation completes.
+// 👉 So you’re logging each request into a file called requests.txt
+
+const myserver=http.createServer((req,res)=>{
+    //jab bhi user request bheje to we do appendfile or we keep a log that when and at what time request was made to server
+    const log=`${Date.now()} ${req.url}:New Request Made\n`;
+    //jab append ho jayega successfully then only we send message to user hello from server 
+//   log is just the message or data you want to record about each request
+    
+    fs.appendFile("requests.txt",log,(err,data)=>{ 
+        //we can create simple routing using url property of req object
+        //humlog yha pe simple routing kar rahe hai using switch case,which checks the url and sends response accordingly
+        switch(req.url){
+            case '/':
+                res.end("Homepage")
+            break;
+            case '/about':
+                res.end("About page")
+            break;
+            default:
+                res.end("404 Page Not Found")
+            break;    
+        }
+    });
+    //console log if any request came to server 
+    console.log("Request has been made from browser to server");
+    //then we end the response using res.end method\
+   
+
+})
+//we need port number for server to listen to requests over a port number 8000
+//if sab kuch is fine then we can see the message in console
+myserver.listen(8000,()=>console.log("Server is listening on port 8000"));
+//8000 was our port number where server is listening to requests

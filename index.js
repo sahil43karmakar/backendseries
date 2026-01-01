523c85c537baa4fa2ca92f9afc162bd59a7f15d3
Sahil karmakar <157059634+sahil43karmakar@users.noreply.github.com>
2026-01-01 18:29:15 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,97 +1,20 @@
-const User=require("../MODELS/User");
-//these function wil use model to perform CRUD operations which we made in User.js file inside MODELS folder
-async function handleGetAllUsers(req, res) {
-    //we take the fucntion of getting all users from mongoose.js
-     const allDbUsers = await User.find({});
-    return res.json(allDbUsers);
-}
-async function handleGetAllUsersbyid(req, res) {
-    const user = await User.findById(req.params.id);
-
-    if (!user) {
-      return res.status(404).json({ error: "❌ User not found" });
-    }
-
-    return res.json(user);
-}
-async function handledeleteUser(req, res) {
- try {
-      const user = await User.findByIdAndDelete(req.params.id);
-
-      if (!user) {
-        return res.status(404).json({ error: "❌ User not found" });
+const fs=require("fs");
+//below is the middleware function which logs the request and response details to a file named log.txt
+//below it s a closure
+function logReqRes(filename){
+    return function(req,res,next){
+           fs.appendFile(
+    "log.txt",
+    `${Date.now()}: ${req.ip} ${req.method}: ${req.path}\n`,
+    (err) => {
+      if (err) {
+        console.error(err);
       }
-
-      return res.json({ message: "✅ User deleted successfully" });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: "⚠️ Internal Server Error" });
+      next();
     }
+  );
 }
-async function handleupdateUser(req, res) {
-  try {
-        //
-      const user = await User.findByIdAndUpdate(
-        req.params.id,
-        req.body,        // 🔥 body se update hoga
-        { new: true }
-      );
-
-      if (!user) {
-        return res.status(404).json({ error: "❌ User not found" });
-      }
-
-      return res.json(user);
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: "⚠️ Internal Server Error" });
-    }
-}
-
-async function handlecreateuser(req, res) {
-  try {
-    console.log("BODY:", req.body);
-
-    const {
-      first_name,
-      last_name,
-      email,
-      gender,
-      jobTitle,   // ✅ correct
-    } = req.body;
-
-    if (!first_name || !last_name || !email || !gender || !jobTitle) {
-      return res.status(400).json({
-        message: "All fields are required"
-      });
-    }
-
-    const user = await User.create({
-      first_name,
-      last_name,
-      email,
-      gender,
-      jobTitle
-    });
-
-    return res.status(201).json({
-      status: "success",
-      id: user._id
-    });
-
-  } catch (error) {
-    console.error("ERROR:", error);
-
-    return res.status(500).json({
-      message: "Server Error",
-      error: error.message
-    });
-  }
-}
+};
 
 
-
-
-
-
-module.exports={handleGetAllUsers,handleGetAllUsersbyid,handledeleteUser,handleupdateUser,handlecreateuser};
+module.exports={logReqRes};
